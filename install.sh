881c7b447fc70768e23225000366feafc6425722
Pooya Dustdar <pdustdar@gmail.com>
2024-11-03 07:43:48 +0000

    add files

---
+++ install.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+version=v24.10.31
+wget https://github.com/XTLS/Xray-core/releases/download/${version}/Xray-linux-64.zip
+unzip Xray-linux-64.zip -d /usr/bin/xray
+rm Xray-linux-64.zip
+mkdir /etc/xray
+touch /etc/xray/config.json
+cp xray.service /etc/systemd/system
+systemctl daemon-reload
+systemctl enable xray
+service xray start
+service xray status
