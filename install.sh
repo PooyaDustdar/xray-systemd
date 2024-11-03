4188eb31cf72ae64656c19b7c15b3d011df46536
Pooya Dustdar <pdustdar@gmail.com>
2024-11-03 07:44:28 +0000

    fix

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ unzip Xray-linux-64.zip -d /usr/bin/xray
 rm Xray-linux-64.zip
 mkdir /etc/xray
 touch /etc/xray/config.json
+useradd xray
+groupadd xray
 cp xray.service /etc/systemd/system
 systemctl daemon-reload
 systemctl enable xray
